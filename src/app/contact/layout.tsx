2355991c8a047f36714ba477a21b0c122204df94
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-08 16:25:59 +0530

    Adding navigation bar

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -22,7 +22,7 @@ export default function HomeLayout({
             <nav className='relative'>
                 <div className='flex p-10 items-center justify-between font-bold'>
                     <div className='flex items-center gap-3'>
-                        <Link href="/" className='flex items-center '>
+                        <Link href="/home" className='flex items-center '>
                             <Image src="/logo.png" alt="Logo" width={35} height={35} className='mr-[-2px]' />
                             <h1 className='text-xl'>ealthify</h1>
                         </Link>
